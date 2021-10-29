3510786be4bc891556139e69c090aace7b19bea8
SaurabhSonde <saurabhsonde111@gmail.com>
2021-10-29 15:51:58 +0530

    helper for authentication

---
+++ apiHelpers/authHelper.js
@@ -0,0 +1,16 @@
+const Api = "http://localhost:5000/api";
+
+export const Signup = (user) => {
+  return fetch(`${Api}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
