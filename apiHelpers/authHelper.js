a3835fc7081977899e46b1a66fc49868ab2321e0
SaurabhSonde <saurabhsonde111@gmail.com>
2021-10-31 21:28:56 +0530

    auth helper

--- apiHelpers/authHelper.js
+++ apiHelpers/authHelper.js
@@ -14,3 +14,52 @@ export const Signup = (user) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const Signin = (user) => {
+  return fetch(`${Api}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//storing jwt in localstorage
+export const authenticate = (data, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("jwt", JSON.stringify(data));
+    next();
+  }
+};
+
+//signout
+export const Signout = (next) => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("jwt");
+    next();
+  }
+
+  return fetch(`${Api}/signout`, {
+    method: "GET",
+  })
+    .then((response) => console.log("signout success"))
+    .catch((err) => console.log(err));
+};
+
+//authentication
+export const isAuthenticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
