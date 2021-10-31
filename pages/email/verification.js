8f4aab65b912d91da1bcb47adb124576da9664a8
SaurabhSonde <saurabhsonde111@gmail.com>
2021-10-31 21:30:50 +0530

    email verification page added

--- pages/email/verification.js
+++ pages/email/verification.js
@@ -1,9 +1,25 @@
 import React from "react";
+import veriStyle from "../../styles/verification.module.css";
 
-const verification = () => {
+const verification = ({ email }) => {
+  console.log(email);
   return (
     <div>
-      <h1>Hiii, check your email.</h1>
+      <div className={veriStyle.headings}>
+        <h1>Verify your email</h1>
+        <span>You will need to verify your email to complete signup.</span>
+        <img src="/mailbox.svg" alt="mailbox" />
+        <div className={veriStyle.para}>
+          <span>
+            An email has been sent to your email address with a link to verify
+            your account.If you have not received the email, please check your
+            spam folder.
+          </span>
+        </div>
+        <div className={veriStyle.buttons}>
+          <button>Contact Support</button>
+        </div>
+      </div>
     </div>
   );
 };
