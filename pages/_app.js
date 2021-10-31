55477b9eb0782874abf1b1efad61765fa046fd3e
SaurabhSonde <saurabhsonde111@gmail.com>
2021-10-31 21:29:46 +0530

    route protection added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { UserContext } from "../components/user";
+import { isAuthenticated } from "../apiHelpers/authHelper";
+import React, { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    if (isAuthenticated) {
+      const user = isAuthenticated();
+      setUser(user);
+    }
+  }, []);
+
+  if (pageProps.protected && !user) {
+    return <h1>You are not signed in.</h1>;
+  }
+  if (pageProps.userTypes && pageProps.userTypes.indexOf(user.type) === -1) {
+    return <h1>Sorry, you don't have access</h1>;
+  }
+  return (
+    <UserContext.Provider value={user}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
