5120fbeedaba1976e9943a4642da2efba177fc57
SaurabhSonde <saurabhsonde111@gmail.com>
2021-11-17 17:29:26 +0530

    added helper

--- apiHelpers/statisticsHelper.js
+++ apiHelpers/statisticsHelper.js
@@ -13,3 +13,17 @@ export const getUserById = (userId, token) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const getMostClickLinks = (userId, token) => {
+  return fetch(`${Api}/most/clicks/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
